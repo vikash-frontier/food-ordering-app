e9c5f8426d09d6fdc516e2aec6a4c7bb10a353bd
vikash-frontier <vikash@ryuklabs.io>
2024-10-03 15:28:24 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className=" text-[#FFF] text-2xl capitalize text-center">
+        Food Ordering app
+      </h1>
+    </>
+  );
+}
+
+export default App;
